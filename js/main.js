4460523d088eda34ef36ff0e0c47a5e4137660d2
keiith001 <156356301+keiith001@users.noreply.github.com>
2024-06-12 13:34:23 -0400

    Background Selection
    This commit is to create a first development phase of the background selection feature.

---
+++ js/main.js
@@ -0,0 +1,25 @@
+console.log("Javascript is connected")
+
+// variables
+const theButtons = document.querySelectorAll("#buttonHolder img");
+const puzzleBoard = document.querySelector(".puzzle-board");
+
+    // console.log(theButtons);
+    // console.log(puzzleBoard);
+
+// function
+function changeBGImage(event) {
+    // console.log("changeBGImage called");
+    // // MEthod 1
+    // console.log(this.id);
+    // // background-image: url('../images/backGround0.jpg');
+    // puzzleBoard.style.backgroundImage = `url('../images/backGround${this.id}.jpg')`;
+
+    // Method 2
+    console.log(event.currentTarget.id);
+    puzzleBoard.style.backgroundImage = `url('../images/backGround${event.currentTarget.id}.jpg')`;
+}
+
+// eventListeners
+
+theButtons.forEach(button => button.addEventListener("click" , changeBGImage));
